7a6646623f696e735e3f2569c67a3d81cf3e8552
justdem <8630623+justdem@users.noreply.github.com>
2023-04-17 14:11:52 -0700

    Sum range only for positive integers in any order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,22 @@
-const sumAll = function(startNum, endNum) {
-    let sum = 0;
-    for (let i = startNum; i <= endNum; i++) {
-        sum = sum + i;
+const sumAll = function(num1, num2) {
+    let startNum;
+    let endNum;
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR'
+    } else {
+        if (num1 <= num2) {
+            startNum = num1;
+            endNum = num2;
+        } else {
+            startNum = num2;
+            endNum = num1
+        };
+        let sum = 0;
+        for (let i = startNum; i <= endNum; i++) {
+            sum = sum + i;
+        }
+        return sum;
     }
-    return sum;
 };
 
 // Do not edit below this line
