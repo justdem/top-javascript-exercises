58d5982a8d12eae607ce22abc268e00961de202d
justdem <8630623+justdem@users.noreply.github.com>
2023-04-17 13:42:08 -0700

    Sum range if first argument less than second

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
