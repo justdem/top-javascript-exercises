41c8b2f84022694e375b47f5fd35fc30de3289df
justdem <8630623+justdem@users.noreply.github.com>
2023-04-16 18:42:41 -0700

    Resolve tests for lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringAsArrayBackwards = []
+    for (let i = string.length; i > 0; i--) {
+        let character = string.charAt(i - 1);
+        stringAsArrayBackwards.push(character);
+    };
+    let backwardsString = stringAsArrayBackwards.toString();
+    return backwardsString.replace(/,/g,"")
 };
+/*
+make string into array
+loop through each letter of string and push to array
+using length of string
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
