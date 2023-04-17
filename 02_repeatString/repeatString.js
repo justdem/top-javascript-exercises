917b5c1af74bdf78c9d66f88f8e3ce5e1c1e84a1
justdem <8630623+justdem@users.noreply.github.com>
2023-04-16 18:10:27 -0700

    Complete lesson 1 and 2 to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatStringResult = "";
+    if (num < 0) {repeatStringResult = 'ERROR'};
+    for (let i = 1; i <= num; i++) {
+        repeatStringResult += string;
+    };
+    return repeatStringResult
 };
 
 // Do not edit below this line
