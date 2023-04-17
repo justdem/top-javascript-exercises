c0f897d8c9400fc0d95d2674200a75826c97823b
justdem <8630623+justdem@users.noreply.github.com>
2023-04-17 13:01:42 -0700

    Pass first test - removing single value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, unwantedValue) {
+    function isNotUnwantedValue(arrayValue) {return arrayValue != unwantedValue};
+    let newArray = array.filter(isNotUnwantedValue);
+    return newArray;
 };
 
 // Do not edit below this line
