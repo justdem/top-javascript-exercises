8fe95fbc2f2b8e71cfd90eee1670b92f31b6c716
justdem <8630623+justdem@users.noreply.github.com>
2023-04-30 14:11:39 -0700

    Complete calc lesson

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((sum, num) => {
+    sum += num;
+    return sum;
+  }, 0)
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((total, num) => {
+    total *= num;
+    return total;
+  }, 1)
+  return total;
 };
 
-const power = function() {
-	
-};
+function power(a, b) {
+  return a**b;
+}
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let i = 1;
+  let factorial = 1;
+  while (i <= a) {
+    factorial *= i;
+    i++;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
