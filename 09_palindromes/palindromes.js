2de890bab2a6026165454688de04f0daaf0d7ec8
justdem <8630623+justdem@users.noreply.github.com>
2023-04-30 16:35:59 -0700

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lower = string.toLowerCase();
+    const alphaNumOnly = lower.match(/[a-z]|[0-9]/g);
+    const length = alphaNumOnly.length;
+    let firstHalf;
+    let secondHalf;
+    if (length % 2 === 0) {
+        firstHalf = alphaNumOnly.slice(0, length/2);
+        secondHalf = alphaNumOnly.slice(length/2);
+    } else {
+        firstHalf = alphaNumOnly.slice(0, Math.floor(length/2));
+        secondHalf = alphaNumOnly.slice(Math.ceil(length/2));
+    };
+    secondHalf.reverse();
+    return secondHalf.join("") === firstHalf.join("");
 };
 
 // Do not edit below this line
