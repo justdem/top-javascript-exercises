fb9d19adad2eb516756615b16c1668a93adcd1f1
justdem <8630623+justdem@users.noreply.github.com>
2023-04-30 17:18:24 -0700

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    let prior = 0;
+    let current = 1;
+    let next;
+    for (i=1; i <= num; i++) {
+        next = prior + current;
+        prior = current;
+        current = next;
+    }
+    return prior;
 };
 
 // Do not edit below this line
